abb12cab5b172820402e262ff2d6c72f6d427adc
leeshinyook <tlsdbr97@gmail.com>
2020-01-02 22:27:36 +0900

    Add : destroy-method속성

--- src/main/java/com/springbook/biz/SamsungTV.java
+++ src/main/java/com/springbook/biz/SamsungTV.java
@@ -7,6 +7,9 @@ public class SamsungTV implements TV {
 	public void initMethod() {
 		System.out.println("객체 초기화 작업처리...");
 	}
+	public void destroyMethod() {
+		System.out.println("객체 삭제 전에 처리할 로직 처리...");
+	}
 	public void powerOn() {
 		System.out.println("SamsungTV --- 전원 켠다.");
 	}
--- src/main/java/com/springbook/biz/TVUser.java
+++ src/main/java/com/springbook/biz/TVUser.java
@@ -10,15 +10,15 @@ public class TVUser {
 		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationcontext.xml"	);
 		
 		
-		// 2. Spring 컨테이너로부터 필요한 객체를 요청(LOOKUP)한다.
-//		TV tv = (TV)factory.getBean("tv");
-//		tv.powerOn();
-//		tv.volumeUp();
-//		tv.volumeDown();
-//		tv.powerOff();
-//		
-//		// 3. Spring 컨테이너를 종료한다.
-//		factory.close();
+//		 2. Spring 컨테이너로부터 필요한 객체를 요청(LOOKUP)한다.
+		TV tv = (TV)factory.getBean("tv");
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
+		
+		// 3. Spring 컨테이너를 종료한다.
+		factory.close();
 	}
 
 }
