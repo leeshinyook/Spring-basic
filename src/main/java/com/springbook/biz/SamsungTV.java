cc73e2e3113a88800cfff9af77ee7f6280d3b75f
leeshinyook <tlsdbr97@gmail.com>
2020-01-02 23:23:11 +0900

    Add : setter 인젝션 활용, (name = set...)

--- src/main/java/com/springbook/biz/SamsungTV.java
+++ src/main/java/com/springbook/biz/SamsungTV.java
@@ -7,6 +7,15 @@ public class SamsungTV implements TV {
 	public SamsungTV() {
 		System.out.println("===> SamsungTV(1) 객체 생성");
 	}
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("===> setSpeaker() 호출.");
+		this.speaker = speaker;
+	}
+	public void setPrice(int price) {
+		System.out.println("===> setPrice() 호출.");
+		this.price = price;
+	}
+	
 	public SamsungTV(Speaker speaker) {
 		System.out.println("===> SamsungTV(2) 객체 생성 ");
 		this.speaker = speaker;
