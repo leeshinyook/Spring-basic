ac730a64afb6fc681173af3b52cd1086f26143d2
leeshinyook <tlsdbr97@gmail.com>
2020-01-04 02:16:08 +0900

    Add : @Resource어노테이션으로 이름기반 의존성주입처리

--- src/main/java/com/springbook/biz/LgTV.java
+++ src/main/java/com/springbook/biz/LgTV.java
@@ -1,5 +1,7 @@
 package com.springbook.biz;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -7,8 +9,8 @@ import org.springframework.stereotype.Component;
 @Component("tv")
 
 public class LgTV implements TV {
-	@Autowired
-	@Qualifier("apple")
+
+	@Resource(name="apple")
 	private Speaker speaker;
 	public void powerOn() {
 		System.out.println("LgTV --- 전원켠다.");
