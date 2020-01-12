ceb9e5bd7133c66949126c63658057c0c0f4d870
leeshinyook <tlsdbr97@gmail.com>
2020-01-13 01:00:55 +0900

    AOP사용전 공통로직에 대한 유연함부족

--- src/main/java/com/springbook/biz/impl/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/impl/BoardServiceImpl.java
@@ -7,29 +7,40 @@ import org.springframework.stereotype.Service;
 
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.common.LogAdvice;
 
 @Service("boardService")
 public class BoardServiceImpl implements BoardService {
 	@Autowired
 	private BoardDAO boardDAO;
+	private LogAdvice log;
+	
+	public BoardServiceImpl() {
+		log = new LogAdvice();
+	}
 	
 	public void insertBoard(BoardVO vo) {
+		log.printLog();
 		boardDAO.insertBoard(vo);
 	}
 
 	public void updateBoard(BoardVO vo) {
+		log.printLog();
 		boardDAO.updateBoard(vo);
 	}
 
 	public void deleteBoard(BoardVO vo) {
+		log.printLog();
 		boardDAO.deleteBoard(vo);
 	}
 
 	public BoardVO getBoard(BoardVO vo) {
+		log.printLog();
 		return boardDAO.getBoard(vo);
 	}
 
 	public List<BoardVO> getBoardList(BoardVO vo) {
+		log.printLog();
 		return boardDAO.getBoardList(vo);
 	}
 
