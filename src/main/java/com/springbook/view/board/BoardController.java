5ba51d48ac5eeafce1eb120c147836f004f0468e
leeshinyook <tlsdbr97@gmail.com>
2020-01-18 21:42:18 +0900

    Add : Method 요청에따른 처리

--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -1,5 +1,52 @@
 package com.springbook.view.board;
 
-public class BoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.impl.BoardDAO;
+
+
+
+// 어노테이션을 이용하여 Controller클래스를 구현하면, 대부분 4~5줄 내외로 간단하게 구현된다.
+// 위를 통합하여 Controller를 하나의 클래스로 구현하면 관리가 더욱 편해진다.
+@Controller
+public class BoardController {
+	// 글등록
+	@RequestMapping("/insertBoard.do")
+	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
+		boardDAO.insertBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	// 글 수정
+	@RequestMapping("/updateBoard.do")
+	public String updateBoard(BoardVO vo, BoardDAO boardDAO) {
+		boardDAO.updateBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	// 글 삭제
+	@RequestMapping("/deleteBoard.do")
+	public String deleteBoard(BoardVO vo, BoardDAO boardDAO) {
+		boardDAO.deleteBoard(vo);
+		return "getBoardList.do";
+	}
+	
+	// 글 상세 조회
+	@RequestMapping("/getBoard.do")
+	public ModelAndView getBoard(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+		mav.addObject("board", boardDAO.getBoard(vo)); // Model 정보 저장
+		mav.setViewName("getBoard.jsp"); // View 정보 저장
+		return mav;
+	}
+	
+	// 글 목록 검색
+	@RequestMapping("/getBoardList.do")
+	public ModelAndView getBoardList(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+		mav.addObject("boardList", boardDAO.getBoardList(vo));
+		mav.setViewName("getBoardList.jsp");
+		return mav;
+	}
 }
